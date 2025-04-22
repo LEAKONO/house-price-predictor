bd8b8fe161e8f87775646662403ce2c488be2ca4
LEAKONO <leakonoemmanuel3@gmail.com>
2025-04-22 17:19:18 +0300

    Add model training script with linear regression

---
+++ model.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import joblib
+
+data = {
+    "Size": [1000, 1500, 2000, 2500, 3000],
+    "Bedrooms": [2, 3, 3, 4, 4],
+    "Price": [300000, 400000, 500000, 600000, 650000]
+}
+
+df = pd.DataFrame(data)
+X = df[["Size", "Bedrooms"]]
+y = df["Price"]
+
+model = LinearRegression()
+model.fit(X, y)
+
+print("Model coefficients:", model.coef_)
+print("Model intercept:", model.intercept_)
+
+# Save model
+joblib.dump(model, "house_model.pkl")
